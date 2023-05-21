9b587067b61b5be67709ee441c2c2132d33e3fa6
Arshat Talap <t_arshat@mail.ru>
2023-05-21 22:49:27 +0600

    Initial Commit

---
+++ arbuzTest/arbuzTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  arbuzTestApp.swift
+//  arbuzTest
+//
+//  Created by Аршат Талап on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct arbuzTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
